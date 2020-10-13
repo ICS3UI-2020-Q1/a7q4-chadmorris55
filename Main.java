2d87ed24b96fba829b22b0c6c116bc406742eda5
chadmorris55 <71134162+chadmorris55@users.noreply.github.com>
2020-10-13 09:46:15 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,12 +4,20 @@
  */
 public class Main {
 
+
+  public static int lastDigit(int number){
+    int last = number % 10;
+    return last;
+  }
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    //calls method
+    int numberTwo = lastDigit(35767);
+    //prints out what was returned
+    System.out.println(numberTwo);
   }
 }
